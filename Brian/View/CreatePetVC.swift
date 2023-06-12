008001fcf386c1bfc3d6f553420210c6f3f5b073
jamesishere00100001 <jattersley@gmail.com>
2023-06-12 17:21:15 +0100

    Functional and progressed to limits of design to date.

--- Brian/View/CreatePetVC.swift
+++ Brian/View/CreatePetVC.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import Photos
 import PhotosUI
 
-class CreatePetVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class CreatePetVC: UIViewController, PHPickerViewControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var headerImage: UIImageView!
     @IBOutlet weak var avatarImage: UIImageView!
+    
     
     @IBOutlet weak var nameTextBox: UITextField!
     @IBOutlet weak var breedTextBox: UITextField!
@@ -19,20 +21,15 @@ class CreatePetVC: UIViewController, UIImagePickerControllerDelegate, UINavigati
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        imagePicker.delegate = self
-        imagePicker.allowsEditing = true
-        imagePicker.sourceType = .photoLibrary
-        
+      
 //        headerImage.layer.masksToBounds = false
 //        headerImage.layer.cornerRadius = headerImage.frame.height/2
 //        headerImage.clipsToBounds = true
         
-        avatarImage.image = UIImage(named: "Profile")
+        avatarImage.image = userPickedImage
         avatarImage.layer.masksToBounds = false
         avatarImage.layer.cornerRadius = headerImage.frame.height/2
         avatarImage.clipsToBounds = true
-        
         
         saveButton.frame = CGRect(x: 0, y: 0, width: 251, height: 51)
         saveButton.backgroundColor = .white
@@ -41,71 +38,102 @@ class CreatePetVC: UIViewController, UIImagePickerControllerDelegate, UINavigati
     
     private var textFieldsFilled: Bool = false
     
-    let imagePicker = UIImagePickerController()
-    
     var petName: String = ""
     var petBreed: String = ""
     var petDOB: String = ""
     
     var saveButton = UIButton()
     
-    var userPickedImage = UIImage()
+    var userPickedImage = UIImage(named: "Profile")
     
-//    imageView.image = userPickedImage
+    //MARK: - Change photo button functionality
     
     @IBAction func changePhotoButton(_ sender: UIButton) {
         
-        present(imagePicker, animated: true)
-        
+        openPhPicker()
     }
     
-    func allTextEntered(status: Bool) {
-        if nameTextBox.hasText && breedTextBox.hasText && dobTextBox.hasText == true {
-            textFieldsFilled = true
-        } else {
-            
-        }
-        
-    }
+    //MARK: - Save button functionality
     
     @IBAction func saveButton(_ sender: UIButton) {
-        if textFieldsFilled == true {
-            petName = nameTextBox.text!
-            petBreed = breedTextBox.text!
-            petDOB = dobTextBox.text!
-            
-            performSegue(withIdentifier: "", sender: self)
-            
-           
-        } else {
-            
-        }
-        print(petName)
-        print(petBreed)
-        print(petDOB)
         
+        allTextEntered()
+        
+        if textFieldsFilled == true {
+            
+            avatarImage.image = resizeImage(image: userPickedImage!, newSize: 200)
+            
+            performSegue(withIdentifier: "goToProfile", sender: self)
+            
+        } else {
+            print("Error, user input not meeting 'textFieldsFilled' criteria")
+        }
     }
     
-    
-    // Add your pet
-//    var view = UILabel()
-//    view.frame = CGRect(x: 0, y: 0, width: 375, height: 50)
-//    view.backgroundColor = .white
-//
-//    view.textColor = UIColor(red: 0.148, green: 0.146, blue: 0.158, alpha: 1)
-//    view.font = UIFont(name: "Shrikhand-Regular", size: 32)
-//    // Line height: 47 pt
-//    view.textAlignment = .center
-//    view.text = "Add your pet"
-//
-//    var parent = self.view!
-//    parent.addSubview(view)
-//    view.translatesAutoresizingMaskIntoConstraints = false
-//    view.widthAnchor.constraint(equalToConstant: 375).isActive = true
-//    view.heightAnchor.constraint(equalToConstant: 50).isActive = true
-//    view.centerXAnchor.constraint(equalTo: parent.centerXAnchor, constant: 0).isActive = true
-//    view.topAnchor.constraint(equalTo: parent.topAnchor, constant: 69).isActive = true
-    
+    //MARK: - Image picker VC and VC launcher
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+             picker.dismiss(animated: true)
+             
+             for result in results {
+                 result.itemProvider.loadObject(ofClass: UIImage.self, completionHandler: {
+                     (object, error) in
+                     if let image = object as? UIImage {
+                         self.userPickedImage = image
+                     }
+                 })
+             }
+         }
+         
+         func openPhPicker() {
+             
+             var config = PHPickerConfiguration()
+             
+             config.selectionLimit = 1
+             config.filter = PHPickerFilter.images
+             
+             let phPickerVC = PHPickerViewController(configuration: config)
+             
+                    phPickerVC.delegate = self
+                    present(phPickerVC, animated: true)
+         }
+        
+        //MARK: - User image resizing to fir avatar imageview
+        
+        func resizeImage(image: UIImage, newSize: CGFloat) -> UIImage {
+            
+            let resizeW = newSize / image.size.width
+            let resizeH = newSize / image.size.height
+            let scaleFactor = min(resizeW, resizeH)
+
+            let scaledImageSize = CGSize(
+                width: image.size.width * scaleFactor, height: image.size.height * scaleFactor)
+
+            let renderer = UIGraphicsImageRenderer(size: scaledImageSize)
+            let newImage = renderer.image { _ in
+                image.draw(in: CGRect(origin: .zero, size: scaledImageSize))
+            }
+            return newImage
+        }
+        
+        //MARK: - Check all user details have been entered to progress to next screen
+        
+        func allTextEntered() {
+            if nameTextBox.hasText && breedTextBox.hasText && dobTextBox.hasText == true {
+                petName = nameTextBox.text!
+                petBreed = breedTextBox.text!
+                petDOB = dobTextBox.text!
+                textFieldsFilled = true
+                
+            } else {
+                let alert = UIAlertController(title: "Uh oh!", message: "You've not finished entering your pet's info", preferredStyle: .alert)
+                let cancelButton = UIAlertAction(title: "Ok", style: .cancel)
+                
+                alert.addAction(cancelButton)
+                
+                present(alert, animated: true)
+            }
+        }
 
 }
 
